cccd35831a15953eba23f86ee90173368e134a12
Mathieu Ledru <matyo91@gmail.com>
2021-06-11 10:14:09 +0200

    :arrow_up: Upgrade deps

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends \Doctrine\Bundle\FixturesBundle\Fixture
+class UserFixtures extends Fixture
 {
     /**
      * AppFixtures constructor.
