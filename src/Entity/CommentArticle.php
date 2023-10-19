3578b4ef8ee7132b2eb40699d736f4be7a1298b7
Mathieu Ledru <mathieu@darkwood.fr>
2023-10-19 10:03:45 +0200

    :construction: Fixes

--- src/Entity/CommentArticle.php
+++ src/Entity/CommentArticle.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: \App\Repository\CommentArticleRepository::class)]
 class CommentArticle extends \App\Entity\Comment
 {
+	#[Assert\NotNull(message: 'common.comment.required_page')]
     #[ORM\ManyToOne(targetEntity: \App\Entity\Article::class, inversedBy: 'comments', cascade: ['persist'])]
     #[ORM\JoinColumn(name: 'article_id', referencedColumnName: 'id')]
     protected ?\App\Entity\Article $article = null;
--- src/Entity/CommentPage.php
+++ src/Entity/CommentPage.php
@@ -5,29 +5,9 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: \App\Repository\CommentPageRepository::class)]
 class CommentPage extends \App\Entity\Comment
 {
-    #[ORM\ManyToOne(targetEntity: \App\Entity\Page::class, inversedBy: 'comments', cascade: ['persist'])]
-    #[ORM\JoinColumn(name: 'page_id', referencedColumnName: 'id')]
-    protected ?\App\Entity\Page $page = null;
-
-    /**
-     * Set page.
-     */
-    public function setPage(Page $page = null): void
-    {
-        $this->page = $page;
-    }
-
-    /**
-     * Get page.
-     *
-     * @return \App\Entity\Page
-     */
-    public function getPage()
-    {
-        return $this->page;
-    }
 }
