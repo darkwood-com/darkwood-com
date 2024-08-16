8973b2a18d12b3f0b5f8bbf16107b961ccd600ee
Mathieu Ledru <mathieu@darkwood.fr>
2024-08-17 01:12:53 +0200

    :white_check_mark: Update CI tests

--- tests/CommonWebTestCase.php
+++ tests/CommonWebTestCase.php
@@ -40,7 +40,6 @@ class CommonWebTestCase extends WebTestCase
 
         $request = $client->getInternalRequest();
 
-        dd($client->getResponse()->getContent());
         self::assertTrue($client->getResponse()->isSuccessful(), sprintf('Page url %s response is not successful', $request->getUri()));
     }
 
