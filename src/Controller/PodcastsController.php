7f5885f543ee9a7c4bd3e0899aeb875a20c766d9
Mathieu Ledru <matyo@darkwood.fr>
2025-02-17 10:48:05 +0100

    ✨ Refactor RSS feed generation with Twig template

--- src/Controller/PodcastsController.php
+++ src/Controller/PodcastsController.php
@@ -27,6 +27,25 @@ class PodcastsController extends AbstractController
         private readonly CacheInterface $cache,
     ) {}
 
+    #[Route(path: ['fr' => '/fr/sitemap.xml', 'en' => '/sitemap.xml', 'de' => '/de/sitemap.xml'], name: 'sitemap_xml')]
+    public function sitemapXml(Request $request)
+    {
+        return $this->commonController->sitemapXml($request);
+    }
+
+    #[Route('/feed.rss', name: 'rss')]
+    public function generateRssFeed(): Response
+    {
+        $podcasts = $this->fetchAndCachePodcasts();
+
+		$response = new Response();
+        $response->headers->set('Content-Type', 'application/xml; charset=utf-8');
+
+		return $this->render('podcasts/pages/feed_rss.html.twig', [
+			'podcasts' => $podcasts,
+		], $response);
+    }
+
     #[Route(path: ['fr' => '/fr/{slug}', 'en' => '/{slug}', 'de' => '/de/{slug}'], name: 'home', defaults: ['ref' => 'home', 'slug' => null])]
     public function home(Request $request, $ref, ?string $slug = null): Response
     {
@@ -54,64 +73,6 @@ class PodcastsController extends AbstractController
             'podcasts' => $podcasts,
 			'podcastLinks' => $podcastLinks,
         ]);
-    }
-
-    #[Route(path: ['fr' => '/fr/sitemap.xml', 'en' => '/sitemap.xml', 'de' => '/de/sitemap.xml'], name: 'sitemap_xml')]
-    public function sitemapXml(Request $request)
-    {
-        return $this->commonController->sitemapXml($request);
-    }
-
-    #[Route('/feed.rss', name: 'rss')]
-    public function generateRssFeed(): Response
-    {
-        $podcasts = $this->fetchAndCachePodcasts();
-
-        // Create the RSS feed
-        $rssFeed = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:googleplay="http://www.google.com/schemas/play-podcasts/1.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/"></rss>');
-
-        // Add XML stylesheet
-        // $rssFeed->addProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="/global/feed/rss.xslt"');
-
-        $channel = $rssFeed->addChild('channel');
-        $channel->addChild('title', 'Darkwood Podcast');
-        $channel->addChild('link', 'https://podcasts.darkwood.com');
-        $channel->addChild('description', 'Darkwood Podcast');
-        $channel->addChild('ttl', '60');
-        $channel->addChild('generator', 'darkwood.com');
-        $channel->addChild('language', 'fr');
-        $channel->addChild('copyright', 'Darkwood');
-        $channel->addChild('itunes:keywords', 'podcast, darkwood');
-        $channel->addChild('itunes:author', 'Darkwood');
-        $channel->addChild('itunes:subtitle', 'Darkwood Podcast Subtitle');
-        $channel->addChild('itunes:summary', 'Darkwood Podcast Summary');
-        $channel->addChild('itunes:explicit', 'false');
-
-        $itunesOwner = $channel->addChild('itunes:owner', null);
-        $itunesOwner->addChild('itunes:name', 'Darkwood');
-        $itunesOwner->addChild('itunes:email', 'matyo91@gmail.com');
-
-        $channel->addChild('itunes:type', 'serial');
-        $channel->addChild('itunes:image', null)->addAttribute('href', 'https://darkwood.com/common/images/site/cover.png');
-
-        foreach ($podcasts as $podcast) {
-            $rssItem = $channel->addChild('item');
-            $rssItem->addChild('title', htmlspecialchars($podcast['title']));
-            $rssItem->addChild('itunes:title', htmlspecialchars($podcast['title']));
-            $rssItem->addChild('link', htmlspecialchars($podcast['audio_file']));
-            $rssItem->addChild('description', htmlspecialchars($podcast['description']));
-            $rssItem->addChild('itunes:summary', htmlspecialchars($podcast['description']));
-            $rssItem->addChild('pubDate', date(DATE_RSS, strtotime($podcast['creation_date'])));
-            // $rssItem->addChild('itunes:duration', '00:00:00'); // Placeholder duration
-            // $rssItem->addChild('enclosure', null)->addAttribute('url', htmlspecialchars($podcast['audio_file']))->addAttribute('type', 'audio/mpeg');
-            $rssItem->addChild('guid', (string) $podcast['id'])->addAttribute('isPermaLink', 'false');
-            $rssItem->addChild('itunes:explicit', 'false');
-        }
-
-        $response = new Response($rssFeed->asXML());
-        $response->headers->set('Content-Type', 'application/xml; charset=utf-8');
-
-        return $response;
     }
 
     private function fetchAndCachePodcasts(): array
